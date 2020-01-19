e07584c158792d7137b1bebbc9837b9de943ef4f
unknown <sergishramchenko@gmail.com>
2020-01-19 22:10:56 +0300

    add chat screen and message composer

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -1,4 +1,5 @@
 import 'package:chatty/models/message.dart';
+import 'package:chatty/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteContacts extends StatelessWidget {
@@ -36,22 +37,30 @@ class FavoriteContacts extends StatelessWidget {
                 padding: EdgeInsets.only(left: 10.0),
                 itemCount: favorites.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 26.0,
-                          backgroundImage:
-                              AssetImage(favorites[index].imageUrl),
-                        ),
-                        SizedBox(height: 6.0),
-                        Text(favorites[index].name,
-                            style: TextStyle(
-                                color: Colors.blueGrey,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w500)),
-                      ],
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => ChatScreen(
+                                  user: favorites[index],
+                                ))),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 26.0,
+                            backgroundImage:
+                                AssetImage(favorites[index].imageUrl),
+                          ),
+                          SizedBox(height: 6.0),
+                          Text(favorites[index].name,
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.w500)),
+                        ],
+                      ),
                     ),
                   );
                 }),
