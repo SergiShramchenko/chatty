bc18a415be2a8f1eefda43f71990638946657fbb
unknown <sergishramchenko@gmail.com>
2020-01-19 10:30:26 +0300

    create an initial structure

---
+++ lib/models/message.dart
@@ -0,0 +1,140 @@
+import 'package:chatty/models/user.dart';
+
+class Message {
+  final User sender;
+  final String
+      time; // Would usually be type DateTime or Firebase Timestamp in production apps
+  final String text;
+  final bool isLiked;
+  final bool unread;
+
+  Message({
+    this.sender,
+    this.time,
+    this.text,
+    this.isLiked,
+    this.unread,
+  });
+}
+
+// YOU - current user
+final User currentUser =
+    User(id: 0, name: 'Current User', imageUrl: 'assets/avatars/Anna.jpg');
+
+// USERS
+final User john =
+    User(id: 1, name: 'John', imageUrl: 'assets/avatars/john.jpg');
+final User elena =
+    User(id: 2, name: 'Elena', imageUrl: 'assets/avatars/elena.jpg');
+final User mark =
+    User(id: 3, name: 'Mark', imageUrl: 'assets/avatars/mark.jpg');
+final User jada =
+    User(id: 4, name: 'Jada', imageUrl: 'assets/avatars/jada.jpg');
+final User steven =
+    User(id: 5, name: 'Steven', imageUrl: 'assets/avatars/steven.jpg');
+final User sophia =
+    User(id: 6, name: 'Sophia', imageUrl: 'assets/avatars/sophia.jpg');
+final User anna =
+    User(id: 7, name: 'Anna', imageUrl: 'assets/avatars/anna.jpg');
+
+// FAVORITE CONTACTS
+List<User> favorites = [anna, steven, jada, elena, mark];
+
+// EXAMPLE CHATS ON HOME SCREEN
+List<Message> chats = [
+  Message(
+    sender: mark,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: jada,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: john,
+    time: '3:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: sophia,
+    time: '2:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: steven,
+    time: '1:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: anna,
+    time: '12:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: elena,
+    time: '11:30 AM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+];
+
+// EXAMPLE MESSAGES IN CHAT SCREEN
+List<Message> messages = [
+  Message(
+    sender: mark,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: true,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '4:30 PM',
+    text: 'Just walked my doge. She was super duper cute. The best pupper!!',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: mark,
+    time: '3:45 PM',
+    text: 'How\'s the doggo?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: mark,
+    time: '3:15 PM',
+    text: 'All the food',
+    isLiked: true,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '2:30 PM',
+    text: 'Nice! What kind of food did you eat?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: mark,
+    time: '2:00 PM',
+    text: 'I ate so much food today.',
+    isLiked: false,
+    unread: true,
+  ),
+];
